8c174543424b463d73705448054663a1c0845912
Augusto Cesar <acs-03@hotmail.com>
2022-03-21 19:15:42 -0300

    GET Department by id

--- src/main/java/com/devsuperior/dsproducts/controllers/DepartmentController.java
+++ src/main/java/com/devsuperior/dsproducts/controllers/DepartmentController.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dsproducts.controllers;
 
 import java.util.List;
+import java.util.UUID;
 
 import com.devsuperior.dsproducts.dto.DepartmentDTO;
 import com.devsuperior.dsproducts.services.DepartmentService;
@@ -8,6 +9,7 @@ import com.devsuperior.dsproducts.services.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +24,12 @@ public class DepartmentController {
     public ResponseEntity<List<DepartmentDTO>> findAll() {
         List<DepartmentDTO> departments = service.findAll();
         return ResponseEntity.ok(departments);
+    }
+    
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<DepartmentDTO> fingById(@PathVariable UUID id) {
+        DepartmentDTO department = service.findById(id);
+        return ResponseEntity.ok(department);
     }
 
 }
--- src/main/java/com/devsuperior/dsproducts/services/DepartmentService.java
+++ src/main/java/com/devsuperior/dsproducts/services/DepartmentService.java
@@ -1,14 +1,17 @@
 package com.devsuperior.dsproducts.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsproducts.dto.DepartmentDTO;
 import com.devsuperior.dsproducts.entities.Department;
 import com.devsuperior.dsproducts.repositories.DepartmentRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import com.devsuperior.dsproducts.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class DepartmentService {
@@ -19,6 +22,12 @@ public class DepartmentService {
     public List<DepartmentDTO> findAll() {
         List<Department> departments = repository.findAll();
         return departments.stream().map(x -> new DepartmentDTO(x)).collect(Collectors.toList());
+    }
+    
+    public DepartmentDTO findById(UUID id) {
+    	Optional<Department> department = repository.findById(id);
+    	Department entity = department.orElseThrow(() -> new ResourceNotFoundException("Department not found!"));
+    	return new DepartmentDTO(entity);
     }
 
 }
